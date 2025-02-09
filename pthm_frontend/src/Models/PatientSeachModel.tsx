61b2503749c4527cd91e01dc1a2bc9926dedf1e5
psu6510110202 <6510110202@email.psu.ac.th>
2025-02-09 18:00:57 +0700

    Create Patient Search Model

---
+++ pthm_frontend/src/Models/PatientSeachModel.tsx
@@ -0,0 +1,12 @@
+export default interface PatientSearchModel {
+    patient_id: string;
+    first_name: string;
+    last_name: string;
+    age: number;
+    gender: string;
+    family_phone: string;
+    family_email: string;
+    address: string;
+    doctor_name: string;
+    room: string;
+}
