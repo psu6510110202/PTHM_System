5b1129134bfd2d47e56f3358a5d7853d3da78ca3
psu6510110202 <6510110202@email.psu.ac.th>
2025-02-19 03:51:30 +0700

    Create PTHM Control Sensor Module

---
+++ esp32/pthm_sensors_hub/pthm_sensors_hub.ino
@@ -0,0 +1,222 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ElegantOTA.h>
+#include <Preferences.h>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+
+#define AP_WIFI_SSID "PTHM_AP"
+#define AP_WIFI_PASS "password"
+
+#define MQTT_SERVER "192.168.215.202"
+#define MQTT_PORT 1883
+#define MQTT_NAME "D000005"
+
+#define DEVICE_ID "000005"
+#define BUTTON_PIN 14
+#define WIFI_TIMEOUT_MS 5000 // 15 seconds timeout for WiFi connection
+
+WebServer server(80);
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+Preferences pref;
+
+
+String STA_WIFI_SSID, STA_WIFI_PASS, PATIENT_ID;
+float BODY_TEMP, BLOOD_OXY;
+int HEART_RATE, ROOM_TEMP, ROOM_HUMIDITY;
+long unsigned previousMillis;
+
+TaskHandle_t buttonTaskHandle = NULL;
+
+void setupWiFi();
+void buttonTask(void *parameter);
+void readNVS();
+void saveNVS();
+void setupWebServer();
+void sendValueMQTT();
+
+const char *index_html PROGMEM = R"(
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8">
+  <title>PTHM Config</title>
+  <style>
+    body { font-family: Arial, sans-serif; background: #f4f4f9; text-align: center; margin: 0; }
+    h1 { color: #333; margin-top: 50px; }
+    a { display: inline-block; padding: 12px 24px; margin: 20px; background: #007BFF; color: #fff; text-decoration: none; border-radius: 5px; }
+    a:hover { background: #0056b3; }
+  </style>
+</head>
+<body>
+  <h1>PTHM Sensor Device Configuration</h1>
+  <a href="/configuration">Setup</a><br>
+  <a href="/update">Firmware Update</a><br>
+</body>
+</html>
+)";
+
+
+const char *conf_html PROGMEM = R"(
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8">
+  <title>PTHM Config</title>
+  <style>
+    body { font-family: Arial, sans-serif; background: #f4f4f9; text-align: center; margin: 0; }
+    h1 { color: #333; margin-top: 50px; }
+    form { display: inline-block; text-align: left; }
+    input[type="text"], input[type="password"] { padding: 10px; margin: 10px; width: 200px; }
+    input[type="submit"] { padding: 10px 20px; background: #007BFF; color: #fff; border: none; border-radius: 5px; cursor: pointer; }
+    input[type="submit"]:hover { background: #0056b3; }
+  </style>
+</head>
+<body>
+  <h1>PTHM Setup Configuration</h1>
+  <form action="/setup_conf" method="POST">
+    <p>SSID: <input type="text" name="SSID"></p>
+    <p>Password: <input type="password" name="PASS"></p>
+    <p>Device ID: %s</p>
+    <p>Patient ID: <input type="text" name="PATIENT_ID" pattern="^\d{6}$" required></p>
+    <input type="submit" value="Save">
+  </form>
+</body>
+</html>
+)";
+
+void setup() {
+  Serial.begin(115200);
+  readNVS();
+  setupWiFi(); // Run WiFi setup synchronously to avoid TCP/IP errors
+  setupMqttClient();
+  setupWebServer();
+  xTaskCreatePinnedToCore(buttonTask, "Button Task", 2048, NULL, 1, &buttonTaskHandle, 1);
+  ElegantOTA.begin(&server);
+  Serial.printf("Device ID: %s | Patient ID: %s\n", DEVICE_ID, PATIENT_ID.isEmpty() ? "[Config Before Use]" : PATIENT_ID.c_str());
+}
+
+void loop() {
+  ElegantOTA.loop();
+  server.handleClient();
+  sendValueMQTT();
+}
+
+void setupWiFi() {
+  unsigned long startAttemptTime = millis();
+  WiFi.begin(STA_WIFI_SSID.c_str(), STA_WIFI_PASS.c_str());
+  if(!STA_WIFI_SSID.isEmpty() && !STA_WIFI_PASS.isEmpty()){
+    Serial.print("Connecting to " + String(STA_WIFI_SSID) + " ");
+  }
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nConnected to Wi-Fi: " + WiFi.localIP().toString());
+  } else {
+    Serial.println("\nWi-Fi connection failed. Starting AP mode...");
+    WiFi.softAP(AP_WIFI_SSID, AP_WIFI_PASS);
+    Serial.println("AP Mode: " + WiFi.softAPIP().toString());
+  }
+}
+
+void readNVS() {
+  pref.begin("Credentials", true);
+  STA_WIFI_SSID = pref.getString("SSID", "");
+  STA_WIFI_PASS = pref.getString("PASS", "");
+  PATIENT_ID = pref.getString("PATIENT_ID", "");
+  pref.end();
+}
+
+void saveNVS() {
+  STA_WIFI_SSID.trim();
+  STA_WIFI_PASS.trim();
+  PATIENT_ID.trim();
+  pref.begin("Credentials", false);
+  pref.putString("SSID", STA_WIFI_SSID);
+  pref.putString("PASS", STA_WIFI_PASS);
+  pref.putString("PATIENT_ID", PATIENT_ID);
+  pref.end();
+}
+
+void setupWebServer() {
+  server.on("/", HTTP_GET, []() { server.send(200, "text/html", index_html); });
+  server.on("/configuration", HTTP_GET, []() { 
+    char rendered[1024];
+    sprintf(rendered, conf_html, DEVICE_ID);
+    server.send(200, "text/html", rendered); 
+  });
+  server.on("/setup_conf", HTTP_POST, []() {
+    STA_WIFI_SSID = server.arg("SSID");
+    STA_WIFI_PASS = server.arg("PASS");
+    PATIENT_ID = server.arg("PATIENT_ID");
+    server.send(200, "text/plain", "PTHM Configuration saved, Restarting...");
+    saveNVS();
+    delay(2000);
+    ESP.restart();
+  });
+  server.begin();
+}
+
+void setupMqttClient(){
+  if (WiFi.status() == WL_CONNECTED && !PATIENT_ID.isEmpty()) {
+    mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
+    Serial.println("Connecting to MQTT broker " + String(MQTT_SERVER) + ":" + String(MQTT_PORT));
+    while (!mqttClient.connected()) {
+      if (mqttClient.connect(MQTT_NAME)) {
+        // String sensorsTopic = "sensor/device/" + String(DEVICE_ID) + "/data";
+        Serial.println("Connected to MQTT broker");
+      } else {
+        Serial.println("Failed to connect to MQTT broker, retrying in 5 seconds...");
+        delay(5000);
+      }
+    }
+  }
+}
+
+void buttonTask(void *parameter) {
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    unsigned long pressStart = 0;
+    // Serial.println("Something wrong here");
+    while (true) {
+      if (digitalRead(BUTTON_PIN) == LOW) {
+        if (millis() - pressStart > 5000) {
+          Serial.println("Clearing preferences and restarting...");
+          pref.begin("Credentials", false);
+          pref.clear();
+          pref.end();
+          delay(2000);
+          ESP.restart();
+        }
+      } else {
+        pressStart = millis();
+      }
+      vTaskDelay(pdMS_TO_TICKS(50));
+    }
+}
+
+void sendValueMQTT(){
+  if(millis() - previousMillis >= 10000){
+    previousMillis = millis();
+
+    int heartRate = random(60, 121);
+    float spo2 = random(9000, 10000) / 100.0;
+    float body_temp = random(2800, 4500) / 100.0;
+    float room_temp = random(1500, 3500) / 100.0;
+    float room_humid = random(5000, 7000) / 100.0;
+
+    StaticJsonDocument<256> doc;
+    doc["heart_rate"] = heartRate;
+    doc["blood_oxy"] = spo2;
+    doc["body_temp"] = body_temp;
+    doc["room_temp"] = room_temp;
+    doc["room_humidity"] = room_humid;
+    doc["patient_id"] = PATIENT_ID;
+    char jsonBuffer[256];
+    serializeJson(doc, jsonBuffer);
+
+    String topic = "sensor/device/" + String(DEVICE_ID);
+    mqttClient.publish(topic.c_str(), jsonBuffer);
+  }
+}
